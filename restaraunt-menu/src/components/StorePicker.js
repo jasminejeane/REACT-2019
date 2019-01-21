8a02cdddcbc02de3fe7873b4350f2ec8b9584355
jasminejeane <jasminejeane@gmail.com>
2019-01-21 15:27:00 -0800

    store picker form

--- restaraunt-menu/src/components/StorePicker.js
+++ restaraunt-menu/src/components/StorePicker.js
@@ -4,8 +4,21 @@ import React from 'react';
 // every component is a class
 class StorePicker extends React.Component {
   // what DOM element do I return to the page
+  // from render you can only return one element
+  // or
+  // <React.Fragment> tag, <div> <> or
+  // import React, {Fragment} from 'react';
+  // then use Fragment instead of React.Fragment
+
   render(){
-    return form.pizza
+    return (
+      // no sibling elements
+      <form action="" className="store-selector">
+      <h2> Please Enter A Store  </h2>
+      <input type="text" required placeholder="Store Name" />
+      <button type="submit"> Visit Store --> </button>
+    </form>
+  )
   }
 }
 

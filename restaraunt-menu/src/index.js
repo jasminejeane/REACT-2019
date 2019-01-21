f1cfea2385c40d5f7d55f583984bf85321ea22ac
jasminejeane <jasminejeane@gmail.com>
2019-01-21 15:41:46 -0800

    change font and bg color.. created form comp

--- restaraunt-menu/src/index.js
+++ restaraunt-menu/src/index.js
@@ -5,6 +5,9 @@ import React from 'react';
 // {} if you want to cherry pick a method from the package
 import { render } from 'react-dom';
 import StorePicker from './components/StorePicker';
-
+import './css/style.css';
 // needs a DOM element to mount onto the page
 render(<StorePicker/>, document.querySelector('#main'));
+
+
+// create react app does all the under the hood webpack

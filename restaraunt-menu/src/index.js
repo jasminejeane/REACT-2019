ca916b3176501ae30d6bfb9c469f58a79cadbc97
jasminejeane <jasminejeane@gmail.com>
2019-01-21 16:12:30 -0800

    inventory and order comp

--- restaraunt-menu/src/index.js
+++ restaraunt-menu/src/index.js
@@ -5,9 +5,10 @@ import React from 'react';
 // {} if you want to cherry pick a method from the package
 import { render } from 'react-dom';
 import StorePicker from './components/StorePicker';
+import App from './components/App';
 import './css/style.css';
 // needs a DOM element to mount onto the page
-render(<StorePicker/>, document.querySelector('#main'));
+render(<App/>, document.querySelector('#main'));
 
 
 // create react app does all the under the hood webpack
